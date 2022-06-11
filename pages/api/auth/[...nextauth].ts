067d29b81daa1aff11b9491d11f86dccd7e6019c
Mihai Burada <mihai.burada@doctarigroup.com>
2022-06-11 14:04:33 +0300

    chore: added Github Auth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,14 @@
 // pages/api/auth/[...nextauth].js
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
-
-import {GOOGLE_OAUTH_CLIENT_SECRET, GOOGLE_OAUTH_CLIENTID, NEXTAUTH_URL} from "../../../utils/config";
+import GithubProvider from "next-auth/providers/github"
+import {
+    GITHUB_OAUTH_CLIENT_SECRET,
+    GITHUB_OAUTH_CLIENTID,
+    GOOGLE_OAUTH_CLIENT_SECRET,
+    GOOGLE_OAUTH_CLIENTID,
+    NEXTAUTH_URL
+} from "../../../utils/config";
 
 const options = {
     site: NEXTAUTH_URL,
@@ -15,9 +21,13 @@ export default NextAuth({
     providers: [
         // OAuth authentication providers..
         GoogleProvider({
-            clientId: GOOGLE_OAUTH_CLIENTID,
+            clientId: GOOGLE_OAUTH_CLIENTID ,
             clientSecret: GOOGLE_OAUTH_CLIENT_SECRET
-        })
+        }),
+        GithubProvider({
+            clientId: GITHUB_OAUTH_CLIENTID,
+            clientSecret: GITHUB_OAUTH_CLIENT_SECRET,
+        }),
     ]
 })
 //export default (req:NextApiRequest, res:NextApiResponse, options:any) => NextAuth(req, res, options)
