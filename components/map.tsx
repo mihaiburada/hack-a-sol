f575d51ba8dd37d6c47ba9efaf70cac5408ef125
Giurgi Bogdan <bogdan.giurgi@doctarigroup.com>
2022-06-11 12:24:02 +0300

    fix: Map and added navigation to location

--- components/map.tsx
+++ components/map.tsx
@@ -1,53 +1,80 @@
-import { useEffect, useRef } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import { useEffect, useRef } from 'react'
+import { useJsApiLoader } from '@react-google-maps/api'
+import { GOOGLE_MAPS_KEY } from '../utils/config'
+import usePlacesService from 'react-google-autocomplete/lib/usePlacesAutocompleteService'
 
-function Map() {
-  const googlemap = useRef(null);
+function Map({ location }: { location: string }) {
+	const googlemap = useRef(null)
 
-  useEffect(() => {
-    const loader = new Loader({
-      apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY as string,
-      version: "weekly",
-      libraries: ["drawing"]
-    });
-    let map;
-    loader.load().then(() => {
-      const google = window.google;
-      map = new google.maps.Map(googlemap.current as any, {
-        center: { lat: -34.397, lng: 150.644 },
-        zoom: 8,
-      });
-      var drawingManager1 = new google.maps.drawing.DrawingManager();
-      console.log(drawingManager1)
-      const drawingManager = new google.maps.drawing.DrawingManager({
-        drawingMode: google.maps.drawing.OverlayType.MARKER,
-        drawingControl: true,
-        drawingControlOptions: {
-          position: google.maps.ControlPosition.TOP_CENTER,
-          drawingModes: [
-            google.maps.drawing.OverlayType.MARKER,
-            google.maps.drawing.OverlayType.CIRCLE,
-            google.maps.drawing.OverlayType.POLYGON,
-            google.maps.drawing.OverlayType.POLYLINE,
-            google.maps.drawing.OverlayType.RECTANGLE,
-          ],
-        },
-        markerOptions: {
-          icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png",
-        },
-        circleOptions: {
-          fillColor: "#ffff00",
-          fillOpacity: 1,
-          strokeWeight: 5,
-          clickable: false,
-          editable: true,
-          zIndex: 1,
-        },
-      });
+	const { isLoaded } = useJsApiLoader({
+		googleMapsApiKey: GOOGLE_MAPS_KEY,
+		version: 'weekly',
+		libraries: ['drawing', 'places']
+	})
 
-      drawingManager.setMap(map);
-    });
-  });
-  return <div id="map" ref={googlemap} />;
+	const { placesService, placePredictions, getPlacePredictions, isPlacePredictionsLoading } = usePlacesService({
+		apiKey: process.env.REACT_APP_GOOGLE
+	})
+
+	useEffect(() => {
+		if (!isLoaded) return
+
+		const compute = async () => {
+			let map
+			const google = window.google
+			map = new google.maps.Map(googlemap.current as any, {
+				center: { lat: -34.397, lng: 150.644 },
+				zoom: 8
+			})
+
+			if (location && placesService) {
+				const getPlacesDetails = (placeId: string): Promise<{ lat: number; lng: number }> => {
+					return new Promise((resolve, reject) => {
+						placesService.getDetails({ placeId }, (result: google.maps.places.PlaceResult | null, status: google.maps.places.PlacesServiceStatus) => {
+							if (status !== 'OK' || !result) {
+								return reject('Eroare la google maps place details')
+							}
+
+              resolve({
+								lat: Number(result.geometry?.location?.lat()),
+								lng: Number(result.geometry?.location?.lng())
+							})
+						})
+					})
+				}
+				const result = await getPlacesDetails(location)
+
+        map = new google.maps.Map(googlemap.current as any, {
+					center: { ...result },
+					zoom: 20
+				})
+			}
+
+			const drawingManager = new google.maps.drawing.DrawingManager({
+				drawingMode: google.maps.drawing.OverlayType.MARKER,
+				drawingControl: true,
+				drawingControlOptions: {
+					position: google.maps.ControlPosition.TOP_CENTER,
+					drawingModes: [google.maps.drawing.OverlayType.MARKER, google.maps.drawing.OverlayType.CIRCLE, google.maps.drawing.OverlayType.POLYGON, google.maps.drawing.OverlayType.POLYLINE, google.maps.drawing.OverlayType.RECTANGLE]
+				},
+				markerOptions: {
+					icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'
+				},
+				circleOptions: {
+					fillColor: '#ffff00',
+					fillOpacity: 1,
+					strokeWeight: 5,
+					clickable: false,
+					editable: true,
+					zIndex: 1
+				}
+			})
+
+			drawingManager.setMap(map)
+		}
+
+		compute()
+	}, [isLoaded, location, placesService])
+	return <div id="map" ref={googlemap} />
 }
-export default Map;
+export default Map
--- utils/config.ts
+++ utils/config.ts
@@ -1,1 +1,2 @@
 export const BASE_URL = 'http://localhost:3000/api'
+export const GOOGLE_MAPS_KEY = 'AIzaSyAAL70plNUSm188wakHJCRU-FWm_mnTpV4'
