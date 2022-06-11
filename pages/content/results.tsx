110d15708a1e01b953d23f6746c1ad4c690431f4
Silviu Manzur <silviumanzur@MacBook-Pro-Silviu.local>
2022-06-11 19:31:41 +0300

    configurable co2 values

--- pages/content/results.tsx
+++ pages/content/results.tsx
@@ -3,16 +3,18 @@ import { Layout } from "antd"
 
 import Visualisation from '../../components/selectionVisualisation'
 import Statistics from "../../components/statistics"
+import { useState } from "react"
 
 const { Sider } = Layout
 
 const ResultsPage: NextPage = () => {
+
   return (
     <Layout style={{ minHeight: "100vh", padding: 24, backgroundColor: 'white', display: 'flex', flexDirection: 'row' }}>
       <div style={{ flex: 1 }}>
         <Statistics />
       </div>
-      <Sider width={600} style={{
+      <Sider width={400} style={{
         borderRadius: 12,
         backgroundColor: 'white',
         boxShadow: '0px 3px 26px -7px rgba(0, 70, 143, 0.5)'
